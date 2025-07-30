eb235368d7187918652e5e4a7a7ac206f37a2081
jlee7362 <jlee736222@gmail.com>
2025-07-30 20:23:37 +0900

    게시글 테스트데이터 생성

--- src/main/java/com/example/demo/controller/UserArticleController.java
+++ src/main/java/com/example/demo/controller/UserArticleController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,16 +11,51 @@ import com.example.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
+	private int lastArticleId;
+	private List<Article> articles;
+	private String title;
+	private String body;
 	
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article (1, "제목1", "내용1");
+	public UserArticleController() {
 		
-		System.out.println(article);
+		this.lastArticleId = 0;
+		this.articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	private void makeTestData() {
+		for(int i = 1; i <=10; i++) {
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			writeArticle(title, body);
+		}		
+	}
+	
+	private Article writeArticle(String title, String body) {
+		int id = ++this.lastArticleId;
+		
+		Article article = new Article(id, title, body);
+		
+		this.articles.add(article);
 		
 		return article;
 	}
+	
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd (String title, String body) {
+		Article article = writeArticle(title,body);
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return this.articles;
+	}
+	
 	
 }
 
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -12,4 +12,5 @@ public class Article {
 	int id;
 	String title;
 	String body;
+	
 }
